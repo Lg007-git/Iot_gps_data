9a1ffcd11fde9c3584fddcf9f32b65808d43e785
Lalit Gupta <lg989212@gmail.com>
2025-04-10 01:06:41 +0530

    CORS Fix-11

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
-//app.options('*', cors());
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
