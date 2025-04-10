8e634e590e1001994bc3b76c56e8d9dd59af43db
Lalit Gupta <lg989212@gmail.com>
2025-04-10 16:25:40 +0530

    CORS Fix-14

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.post('/gps/v1', async (req, res) => {
   }
 });
 
+app.get('/gps/v1', (req, res) => {
+  res.send('POST your GPS data here.');
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running');
 });
