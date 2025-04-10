ccd34ab419f892e75f8f0b0e370dbb53bc1a70fe
Lalit Gupta <lg989212@gmail.com>
2025-04-11 00:17:53 +0530

    cors final2

--- frontend_p/src/App.jsx
+++ frontend_p/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
         try {
           //await fetch("https://iot-gps-data-backend.vercel.app/gps/v1", {
-          await fetch("https://iot-gps-data-20gqembtb-lg007s-projects.vercel.app/gps/v1",{
+          await fetch("https://iot-gps-data-gq1r.vercel.app/gps/v1",{
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(gpsData)
