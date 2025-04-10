9ba8f0c8ac241aa75e13a36fac462576133bcdef
Lalit Gupta <lg989212@gmail.com>
2025-04-10 18:03:18 +0530

    cors 17

--- backend/api/gps.js
+++ backend/api/gps.js
@@ -1,30 +1,42 @@
 const mongoose = require('mongoose');
 const GPS = require('../models/GpsData');
-require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+const corsOptions = {
+  origin: ['https://iot-gps-data.vercel.app'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') return res.status(200).end();
-
-  if (req.method === 'POST') {
-    try {
-      const gps = new GPS(req.body);
-      await gps.save();
-      res.status(200).send('Saved to MongoDB');
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  } else if (req.method === 'GET') {
-    res.status(200).send('POST your GPS data here.');
-  } else {
-    res.status(405).send('Method Not Allowed');
+// POST handler
+app.post('/api/gps/v1', async (req, res) => {
+  try {
+    const gps = new GPS(req.body);
+    await gps.save();
+    res.status(200).send('Saved to MongoDB');
+  } catch (err) {
+    res.status(500).send(err.message);
   }
-};
+});
+
+// GET handler
+app.get('/api/gps/v1', (req, res) => {
+  res.send('POST your GPS data here.');
+});
+
+// For Vercel serverless
+module.exports = app;
